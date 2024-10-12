02f22f771c538e0fff9301b2566a7ab290770261
AGH <cccloud1016@gamli.com>
2024-10-12 23:11:34 +0900

    [create] [Class] [ArrayList] [basic]

---
+++ java_practice/src/java_1012/ArrayList_basic.java
@@ -0,0 +1,19 @@
+package java_1012;
+
+import java.util.ArrayList;
+
+public class ArrayList_basic {
+
+	public static void main(String[] args) {
+		ArrayList<String> fruits = new ArrayList<>();
+		
+		fruits.add("apple");
+		fruits.add("orange");
+		fruits.add("cherry");
+		fruits.add("banana");
+		fruits.add("avocado");
+		
+		System.out.println(fruits);
+	}
+
+}
